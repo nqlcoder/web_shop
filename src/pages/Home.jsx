afb520c78ceeb98b574a3a84c30b7645ba8ca9c6
ling <nqlcoder@gmail.com>
2022-08-13 11:08:08 +0700

    make hero slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Helmet from "../components/Helmet";
+import HeroSlider from "../components/HeroSlider";
+import heroSliderData from "../assets/fake-data/hero-slider";
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <Helmet title="Trang chủ">
+            {/* hero slider */}
+            <HeroSlider data={heroSliderData} control={true} />
+            {/* end hero slider */}
+        </Helmet>
+    );
 };
 
 export default Home;
